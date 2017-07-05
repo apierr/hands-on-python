1c7a69322270e824ea76dc6c84a970dd7b224cd6
Antonio Pierro <antonio.pierro@gmail.com>
2017-07-05 12:20:47 +0200

    bisection search using max() function

--- while-list/square-root-approximation.py
+++ while-list/square-root-approximation.py
@@ -18,12 +18,12 @@ square_root(12345)
 
 # Bisection search: cut search space in half each iteration
 
-def square_root_log(x = 2, epsilon = 0.01, stepSize = 0.00001):
+def square_root_log(x = 2, epsilon = 0.01):
     numGuesses = 0
     low = 0.0
-    high = x
+    high = max(x, 1.0) # When x is between 0 and 1, the square root of x is greater than x itself.
     ans = (high + low) / 2.0
-    while abs(ans ** 2 - x) >= epsilon and ans <= x:
+    while abs(ans * ans - x) >= epsilon and ans <= x:
         numGuesses += 1
         if ans ** 2 < x:
             low = ans
@@ -34,3 +34,4 @@ def square_root_log(x = 2, epsilon = 0.01, stepSize = 0.00001):
     print ans, 'is close to square root of', x
 
 square_root_log(12345)
+square_root_log(0.5)

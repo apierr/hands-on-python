622ec30591b4b164b714e305c79cf8d972249683
Antonio Pierro <antonio.pierro@gmail.com>
2017-07-11 12:10:24 +0200

    Added recursive power function

--- mit/lect6.py
+++ mit/lect6.py
@@ -1,6 +1,18 @@
 """
 Recursion
 """
+
+def simpleExp(b, n):
+    """
+    Recursive power function
+    """
+    if n == 0:
+        return 1
+    return b * simpleExp(b, n - 1)
+
+print simpleExp(2, 2)
+print simpleExp(2, 8)
+
 def hanoi(n, source, helper, target):
     """
     The Tower of Hanoi problem with 3 pegs and n disks takes  2**n - 1 moves to solve
@@ -16,4 +28,20 @@ def hanoi(n, source, helper, target):
         hanoi(1, source, helper, target)
         hanoi(n - 1, helper, source, target)
 
-hanoi(2, 'source', 'temp', 'target')
+# hanoi(2, 'source', 'temp', 'target')
+
+def toChars(s):
+    import string
+    return string.lower(s)
+
+def isPal(s):
+    if len(s) <= 1:
+        return True
+    else:
+        return s[0] == s[-1] and isPal(s[1:-1])
+
+def isPalindrome(s):
+    """Returns True if s is a palindrome and False otherwise"""
+    return isPal(toChars(s))
+
+#print isPalindrome('Anna Anna')
